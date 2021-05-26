f98cc424f82abfa2965262da6251500a9d1590ad
AdrianKorkus <adrian.korkus.2017@gmail.com>
2021-05-26 19:53:38 +0200

    Add funkcje.py.

---
+++ funkcje.py
@@ -0,0 +1,9 @@
+def leppard():
+    refren = "Oh, I get hysterical, hysteria, oh, can you feel it, do you believe it?"
+    refren += "\nIt's such a magical mysteria"
+    print(refren)
+
+
+x = leppard()
+print(type(x))
+print(x)
