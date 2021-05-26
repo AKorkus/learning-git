5b81d0d10a32e2577444ba036a761a4e8b51a879
AdrianKorkus <adrian.korkus.2017@gmail.com>
2021-05-26 20:18:25 +0200

    Extend funkcje.py.

--- funkcje.py
+++ funkcje.py
@@ -1,9 +1,35 @@
+# Testowe:
 def leppard():
     refren = "Oh, I get hysterical, hysteria, oh, can you feel it, do you believe it?"
     refren += "\nIt's such a magical mysteria"
     print(refren)
 
 
-x = leppard()
-print(type(x))
-print(x)
+def inne():
+    print("x")
+
+
+def kaller(funkcja=inne):
+    funkcja()
+
+
+# kaller(leppard)
+#x = leppard()
+# print(type(x))
+# print(x)
+
+# Generator Wielomianów:
+
+
+def oblicz(x, *args):
+    power = len(args)
+    y = 0
+    print(len(args))
+    for i in args:
+        y += i*x**power
+        power -= 1
+    return y
+
+
+print(oblicz(2, 3, -2, 1))
+print("chuj")
